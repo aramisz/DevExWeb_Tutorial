dbf163d8495ffcb84618cd2b24f2a3116ae385f6
aramisz <aramisz@artscript.eu>
2015-03-18 15:03:18 +0100

    Image directive with 2 variations

--- client/app/directive/as-img.js
+++ client/app/directive/as-img.js
@@ -12,7 +12,37 @@ app.directive('asImg', function () {
         link: function (iScope, iElement, iAttrs) {
             var src = iAttrs.src;
             var src_hover = iAttrs.srcHover;
-            var active = iAttrs.active != undefined ? true : false;
+            var active = iAttrs.active;
+            var active = angular.isDefined(active) && active != 'false' ? true : false;
+
+            iElement.on('mouseover click', function () {
+                iElement.attr('src', src_hover);
+            });
+
+            iElement.on('mouseout', function () {
+                if (!active) {
+                    iElement.attr('src', src);
+                }
+            });
+
+            if (active) {
+                iElement.attr('src', src_hover);
+            }
+        }
+    }
+});
+
+app.directive('asImageHover', function () {
+    return {
+        restrict: 'A',
+        scope: '@',
+        template: '<img>',
+        replace: true,
+        link: function (iScope, iElement, iAttrs) {
+            var src = iAttrs.src;
+            var src_hover = iAttrs.asImageHover;
+            var active = iAttrs.active;
+            var active = angular.isDefined(active) && active != 'false' ? true : false;
 
             iElement.on('mouseover click', function () {
                 iElement.attr('src', src_hover);
